98b862b223f9c50eb680f62c92ed12734f15a118
Ezhov Alexey <ezhov-a@yandex.ru>
2021-02-18 06:14:26 +0300

    Добавляет главную страницу + js к аккордеону и слайдеру

--- gulpfile.js
+++ gulpfile.js
@@ -48,7 +48,7 @@ gulp.task('server', function () {
   gulp.watch('source/sass/**/*.{scss,sass}', gulp.series('css'));
   gulp.watch('source/img/**/{icon-*,logo-*,logo}.svg', gulp.series('sprite', 'html', 'refresh'));
   gulp.watch('source/*.html', gulp.series('html', 'refresh'));
-  gulp.watch('source/js/**/*.js', gulp.series('js'));
+  gulp.watch('source/js/**/*.js', gulp.series('js', 'vendor'));
 });
 
 gulp.task('refresh', function (done) {
@@ -117,7 +117,6 @@ gulp.task('copy', function () {
   return gulp.src([
     'source/fonts/**/*.{woff,woff2}',
     'source/img/**',
-    'source/js/**',
     'source//*.ico',
   ], {
     base: 'source',
--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,43 @@
 const pageClass = {
   HEADER_NOJS: 'header--nojs',
+  MAIN_CONTAINER_NOJS: 'main__container--nojs',
+  FAQ_NOJS: 'faq--nojs',
   HEADER_OPEN: 'header--open',
   BTN_MENU: 'js-btn-menu',
+  FAQ_ACTIVE: 'faq__item--active'
 };
 
 const header = document.querySelector('.js-header');
+const mainContainer = document.querySelector('.js-main-container');
 const btnMenu = document.querySelector('.js-btn-menu');
+const faqBlock = document.querySelector('.js-faq');
+const accordionBtn = document.querySelectorAll('.js-btn-faq');
+const accordionItems = document.querySelectorAll('.faq__item');
 
 const toggleMenuHandler = () => {
   header.classList.toggle(pageClass.HEADER_OPEN);
 };
 
 header.classList.remove(pageClass.HEADER_NOJS);
+mainContainer.classList.remove(pageClass.MAIN_CONTAINER_NOJS);
+faqBlock.classList.remove(pageClass.FAQ_NOJS);
+
+const toggleAccordionHandler = (evt) => {
+  const target = evt.target.parentElement;
+  if (target.classList.contains(pageClass.FAQ_ACTIVE)) {
+    target.classList.remove(pageClass.FAQ_ACTIVE);
+  } else {
+    for (let item of accordionItems) {
+      if (item.classList.contains(pageClass.FAQ_ACTIVE)) {
+        item.classList.remove(pageClass.FAQ_ACTIVE);
+      }
+    }
+    target.classList.add(pageClass.FAQ_ACTIVE);
+  }
+};
+
+for (let btn of accordionBtn) {
+  btn.addEventListener('click', toggleAccordionHandler);
+}
 
 btnMenu.addEventListener('click', toggleMenuHandler);
