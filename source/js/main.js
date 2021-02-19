4e2eea967784d40f9d148d64bc48798a78db13b4
Ezhov Alexey <ezhov-a@yandex.ru>
2021-02-19 18:15:32 +0300

    Интерактив для Catalog + nojs

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 (() => {
   const pageClass = {
+    PAGE__LOCKED: 'page--locked',
     HEADER_NOJS: 'header--nojs',
     MAIN_CONTAINER_NOJS: 'main__container--nojs',
     FILTER_INPUT_NOJS: 'filter__inputs--nojs',
@@ -7,8 +8,10 @@
     BTN_MENU: 'js-btn-menu',
     ACCORDION_CLASS: 'js-btn-accordion',
     ACTIVE_CLASS: '--active',
+    FILTER_ACTIVE: 'filter--active',
   };
 
+  const page = document.querySelector('.js-page');
   const header = document.querySelector('.js-header');
   const mainContainer = document.querySelectorAll('.js-main-container');
   const btnMenu = document.querySelector('.js-btn-menu');
@@ -16,6 +19,10 @@
   const accordionBtn = document.querySelectorAll('.js-btn-accordion');
   const accordionItems = document.querySelectorAll('.js-accordion-item');
   const rangeInput = document.querySelector('.js-range-input');
+  const filterBlock = document.querySelector('.js-filter');
+  const filterForm = document.querySelector('.js-filter-form');
+  const filterOpenBtn = document.querySelector('.js-btn-filter-open');
+  const filterCloseBtn = document.querySelector('.js-btn-filter-close');
 
   const toggleMenuHandler = () => {
     header.classList.toggle(pageClass.HEADER_OPEN);
@@ -38,6 +45,20 @@
     }
   };
 
+  const closeFilterHandler = () => {
+    filterBlock.classList.remove(pageClass.FILTER_ACTIVE);
+    page.classList.remove(pageClass.PAGE__LOCKED);
+    filterOpenBtn.addEventListener('click', openFilterHandler);
+    filterCloseBtn.removeEventListener('click', closeFilterHandler);
+  };
+
+  const openFilterHandler = () => {
+    filterBlock.classList.add(pageClass.FILTER_ACTIVE);
+    page.classList.add(pageClass.PAGE__LOCKED);
+    filterCloseBtn.removeEventListener('click', closeFilterHandler);
+    filterCloseBtn.addEventListener('click', closeFilterHandler);
+  };
+
   if (accordionBlock) {
     for (let btn of accordionBtn) {
       btn.addEventListener('click', toggleAccordionHandler);
@@ -51,6 +72,14 @@
     item.classList.remove(pageClass.MAIN_CONTAINER_NOJS);
   }
   header.classList.remove(pageClass.HEADER_NOJS);
+
+  if (filterOpenBtn) {
+    filterOpenBtn.addEventListener('click', openFilterHandler);
+  }
+
+  if (filterForm) {
+    filterForm.addEventListener('submit', closeFilterHandler);
+  }
 
   btnMenu.addEventListener('click', toggleMenuHandler);
 })();
